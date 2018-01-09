316ff3ca09674712709f4b6412a553f4f36ed51e
Mikko Lakomaa <hopealusikka@gmail.com>
2018-01-09 23:12:42 +0200

    Various changes to app.tsx

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
-import Header from './header';
+import { Route } from 'react-router-dom'
+
+import Header from './header/header';
 import { AxeFx } from '../api/axefx';
 import { GenericMIDIController } from '../api/generic-midi-controller';
 import { PanelObject } from '../api/panel-object';
 import Sidebar from './sidebar/sidebar';
+import PanelContainer from '../containers/panel-container';
 
 interface AppProps {
     init: () => void;
+    addNewPanel: () => void;
     axeFx: AxeFx;
     controller: GenericMIDIController;
     firmwareVersion: string;
@@ -20,11 +24,12 @@ export class AppComponent extends React.Component<AppProps> {
         this.props.init();
     }
     render() {
-        const { axeFx, controller, firmwareVersion, presetName, panels } = this.props;
-        return <div className="app">
+        const { addNewPanel, axeFx, controller, firmwareVersion, presetName, panels } = this.props;
+        return <div id="app">
                 <Header firmwareVersion={firmwareVersion} presetName={presetName} axeFx={axeFx} controller={controller}></Header>
-                <Sidebar panels={panels}></Sidebar>
+                <Sidebar addNewPanel={addNewPanel} panels={panels}></Sidebar>
                 <div className="main-container">
+                    <Route path="panels/:panelId" component={PanelContainer} />
                 </div>
             </div>;
     }
