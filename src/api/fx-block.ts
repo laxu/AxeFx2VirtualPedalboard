5768330f73aba0450459abbd0e2d83283dcee1ae
Mikko Lakomaa <hopealusikka@gmail.com>
2018-01-12 01:38:58 +0200

    Add block builder and more params to fx block

--- src/api/fx-block.ts
+++ src/api/fx-block.ts
@@ -1,19 +1,19 @@
 import { clampValue, getObjKeyByValue } from "../util/util";
-import { FX_PARAM_IDS, FX_PARAM_LABELS } from "./fx-block-data";
+import { FX_BLOCK_IDS, FX_BLOCK_TYPES, FX_PARAMS, PARAM_TYPE } from "./fx-block-data";
 
 export interface IFxBlock {
     id: number;
-    engaged: boolean;
-    isX: boolean;
+    engaged?: boolean;
+    isX?: boolean;
     parameters: IFxParam[];
 }
 
 export interface IFxParam {
     blockGroup: string;
     id: number;
+    type: PARAM_TYPE;
     value: number;
     step?: number;
-    cc?: number;
     range?: [number, number];
     label?: string;
 }
@@ -48,16 +48,18 @@ export class FxBlock implements IFxBlock {
 export class FxParam implements IFxParam {
     blockGroup: string;
     id: number;
+    type: PARAM_TYPE;
     step?: number;
-    cc?: number;
     range?: [number, number];
     label?: string;
 
     constructor(paramSettings: IFxParam) {
+        this.blockGroup = paramSettings.blockGroup;
+        this.type = paramSettings.type;
         this.id = paramSettings.id;
         this.step = paramSettings.step || 0.1;
         this.range = paramSettings.range || [0,10];
-        this.label = FX_PARAM_LABELS[this.blockGroup + this.id];
+        this.label = paramSettings.label;
     }
 
     set value(val: number) {
@@ -65,3 +67,23 @@ export class FxParam implements IFxParam {
         this.value = clampValue(val, this.range, this.step);
     }
 }
+
+// Build blocks
+const blocks = [];
+
+for (const blockType of FX_BLOCK_TYPES) {
+    let counter = 0;
+    let blockName = blockType + counter;
+    while(FX_BLOCK_IDS[blockName]) {
+        const id = FX_BLOCK_IDS[blockName];
+        let parameters = [];
+        if (FX_PARAMS[blockType]) {
+            parameters = FX_PARAMS[blockType].map(param => new FxParam({ blockGroup: blockType, ...param }));
+        }
+        blocks.push(new FxBlock({ id, parameters }));
+        counter++;
+        blockName = blockType + counter;
+    }
+}
+
+export default blocks;
