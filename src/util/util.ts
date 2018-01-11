be8945a46fa84ce32cffc509e12ebb25daef4055
Mikko Lakomaa <hopealusikka@gmail.com>
2018-01-12 01:40:21 +0200

    Various tweaks to util

--- src/util/util.ts
+++ src/util/util.ts
@@ -9,31 +9,35 @@ export function getObjKeyByValue(val: any, obj: any): string {
     return null;
 }
 
-export function intTo2Byte(val: number) {
+export function intTo2Byte(val: number): Uint8Array {
     const byte1 = val & 0x7F;
     const byte2 = val >> 7;
     return Uint8Array.from([byte1, byte2]);
 }
 
-export function bytes2ToInt(bytes: Uint8Array) {
+export function bytes2ToInt(bytes: Uint8Array): number {
     return (bytes[0] & 0x7F) << 7 | bytes[1];
 }
 
-export function parameterValueBytesToInt(byteArray) {
+export function parameterValueBytesToInt(byteArray): number {
     return (byteArray[0] & 0x7F) | ((byteArray[1] & 0x7F)<<7) | ((byteArray[2] & 0x7F)<<14);
 }
 
-export function parameterValueIntToBytes(value: number) {
+export function parameterValueIntToBytes(value: number): Uint8Array {
     const byte1 = value & 0x7F;
     const byte2 = (value >> 7) & 0x7F;
     const byte3 = (value >> 14) & 0x7F;
     return Uint8Array.from([byte1, byte2, byte3]);
 }
 
-export function clampValue(val: number, range: [number, number], step: number) {
+export function clampValue(val: number, range: [number, number], step: number): number {
     let value = Math.min(Math.max(range[0], val), range[1]);
     if (step && value % step > 0) {
         value = value - value % step;
     }
     return value;
+}
+
+export function isElectron(): boolean {
+    return navigator.userAgent.toLowerCase().indexOf(' electron/') > -1;
 }
