1f76f86648514b68d01f4df38a592577afbd18ec
Mikko Lakomaa <hopealusikka@gmail.com>
2018-01-12 01:38:38 +0200

    Various redux persist updates to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,40 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import { PersistGate } from 'redux-persist/lib/integration/react'
+import storage from 'redux-persist/lib/storage';
 
-import { reducers } from "./store/reducers";
-import { createStore } from "redux";
+import reducers from './store/reducers';
+import { createStore } from 'redux';
 import { Provider, Store } from 'react-redux';
 import AppContainer from './containers/app-container';
-import { WebMidiWrapper } from "./api/midi";
+import { WebMidiWrapper } from './api/midi';
+import { isElectron } from './util/util';
 import './styles/main.scss';
 
+const config = {
+    key: 'axeFxMidi',
+    storage: storage
+}
+
+const storeReducer = persistCombineReducers(config, {app: reducers});
+
 const store: Store<any> = createStore(
-    reducers,
+    storeReducer,
     // @ts-ignore
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // t
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+const persistor = persistStore(store);
+
 WebMidiWrapper.init(() => {
     ReactDOM.render(
         <Provider store={store}>
-            <Router>
-                <AppContainer></AppContainer>
-            </Router>
+            <PersistGate persistor={persistor}>
+                <Router>
+                    <AppContainer></AppContainer>
+                </Router>
+            </PersistGate>
         </Provider>,
         document.getElementById('app-container')
     );
