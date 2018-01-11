9671c0fe0bafc878545b1c6a7c23a3d03816b9c1
Mikko Lakomaa <hopealusikka@gmail.com>
2018-01-12 01:40:52 +0200

    Implement more of panel container

--- src/containers/panel-container.tsx
+++ src/containers/panel-container.tsx
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 
-import { getPanelAction } from "../store/actions";
+import { getPanelAction, setPanelAction } from "../store/actions";
 import { WebMidiWrapper, MIDIController, MIDIListenerType } from "../api/midi";
 import { MODEL_IDS } from "../api/constants";
 import { AxeFx } from "../api/axefx";
@@ -11,24 +11,41 @@ import { ControlType, ControlObject } from '../api/control-object';
 import PanelComponent from '../components/panel/panel';
 
 const mapStateToProps = state => ({
-    panel: state.currentPanel
+    panel: state.app.currentPanel
 });
 
 const mergeProps = (stateProps, dispatchProps, ownProps) => ({
     ...stateProps,
     ...dispatchProps,
-    init() {
+    ...ownProps,
+    init(panelId) {
         const { dispatch } = dispatchProps;
-        const { match } = ownProps;
-        dispatch(getPanelAction(match.params.panelId));
+        dispatch(getPanelAction(panelId));
+    },
+    savePanelChanges(formValues) {
+        const { panel } = stateProps;
+        const { dispatch } = dispatchProps;
+        const updatedPanel = Object.assign({}, panel, formValues);
+        console.log('fuu', updatedPanel);
+        dispatch(setPanelAction(updatedPanel));
+        dispatch(getPanelAction(updatedPanel.id));
     },
     addPanelControl(controlType: ControlType) {
+        const { panel } = stateProps;
         const control: ControlObject = {
+            id: panel.controls.length + 1,
             block: null,
             param: null,
-            controlType
-        }
-        stateProps.panel.controls.push(control);
+            controlType,
+            cc: null
+        };
+        panel.controls.push(control);
+        console.log('fuu', panel.controls);
+    },
+    removePanelControl(control: ControlObject) {
+        const { panel } = stateProps;
+        const controlIdx = panel.controls.find(ctrl => ctrl.id === control.id);
+        panel.controls.splice(controlIdx, 1);
     }
 });
 
