7124960708de9726fc46c09ae2197c4ad694b03d
Mikko Lakomaa <hopealusikka@gmail.com>
2018-01-13 17:16:57 +0200

    Make header look nicer

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -14,22 +14,36 @@ export default class HeaderComponent extends React.Component<Props> {
     openSettings() {}
     render() {
         const { axeFx, controller, firmwareVersion, presetName } = this.props;
-        const axeFxName = axeFx ? axeFx.name : null;
-        const controllerName = controller ? controller.name : null;
         
         return (
             <div className="header">
-                <div className="logo">Axe-Fx MIDI</div>
+                <div className="logo">Axe-Fx MIDI Middleman</div>
                 <div className="info">
                     <div className="axe-fx-info">
                         <h4>Axe-Fx</h4>
-                        <div className="device-name">Device name: <span>{axeFxName}</span></div>
-                        <div className="firmware-version">Firmware version: <span>{firmwareVersion}</span></div>
-                        <div className="presetName">Preset: <span>{presetName}</span></div>
+                        <div className="info-container">
+                            <div className="device-name">
+                                <label>Device name:</label>
+                                <span>{controller && controller.name}</span>
+                            </div>
+                            <div className="firmware-version">
+                                <label>Firmware version:</label>
+                                <span>{firmwareVersion}</span>
+                            </div>
+                            <div className="presetName">
+                                <label>Preset:</label>
+                                <span>{presetName}</span>
+                            </div>
+                        </div>
                     </div>
                     <div className="controller-info">
                         <h4>MIDI Controller</h4>
-                        <div className="device-name">Device name: <span>{controllerName}</span></div>
+                        <div className="info-container">
+                            <div className="device-name">
+                                <label>Device name:</label>
+                                <span>{axeFx && axeFx.name}</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="actions">
