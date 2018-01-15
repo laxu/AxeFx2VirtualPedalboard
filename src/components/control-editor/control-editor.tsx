3d53169fdd6c47ad9efbb7b290405d2e33ddf850
Mikko Lakomaa <hopealusikka@gmail.com>
2018-01-15 18:52:48 +0200

    Make control editor work

--- src/components/control-editor/control-editor.tsx
+++ src/components/control-editor/control-editor.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { ControlType, ControlObject } from '../../api/control-object';
 import './_control-editor.scss';
-import { FxBlock, getAllBlocks, getBlockById } from '../../api/fx-block';
+import { FxBlock, getAllBlocks, getBlockById, FxParam } from '../../api/fx-block';
 
 interface Props {
-    controlId: number;
-    blockId: number;
-    paramId: number;
+    id: string;
+    block: FxBlock;
+    param: FxParam;
     controlType: ControlType;
     cc: number;
     saveChanges: (ControlObject) => void;
@@ -19,19 +19,22 @@ interface State {
     paramId: number;
     controlType: ControlType;
     cc: number;
-    [key: string]: any
+    hasChanges: boolean;
+    [key: string]: any;
 }
 
 export default class ControlEditorComponent extends React.Component<Props, State> {
     constructor(props) {
         super(props);
-        console.log('params', this.props);
+        
+        const { block, param, controlType, cc } = this.props;
         this.state = {
             blocks: getAllBlocks(),
-            blockId: this.props.blockId,
-            paramId: this.props.paramId,
-            controlType: this.props.controlType || ControlType.Control,
-            cc: this.props.cc
+            blockId: block && block.id,
+            paramId: param && param.id,
+            controlType: controlType || ControlType.Control,
+            cc: cc,
+            hasChanges: false
         };
 
         this.onSubmit = this.onSubmit.bind(this);
@@ -40,10 +43,10 @@ export default class ControlEditorComponent extends React.Component<Props, State
     onSubmit(event) {
         event.preventDefault();
         const { blockId, paramId, cc, controlType } = this.state;
-        const { controlId, closeModal, saveChanges } = this.props;
+        const { id, closeModal, saveChanges } = this.props;
         if (!blockId || !paramId) return false;
         saveChanges({
-            controlId,
+            id,
             blockId,
             paramId,
             controlType,
@@ -59,24 +62,23 @@ export default class ControlEditorComponent extends React.Component<Props, State
 
     setValue(prop: string, value: string) {
         this.setState({
-            [prop]: prop === 'controlType' ? value : Number(value)
+            [prop]: prop === 'controlType' ? value : Number(value),
+            hasChanges: true
         });
     }
 
     render() {
-        const { controlId } = this.props;
-        const { blocks, blockId, paramId, cc, controlType } = this.state;
+        const { hasChanges, blocks, blockId, paramId, cc, controlType } = this.state;
 
-        console.log('all blocks', blocks);
         const selectedBlock: FxBlock = blockId ? getBlockById(blockId) : null;
         
         return (
             <form className="form control-editor" autoComplete="off" onSubmit={this.onSubmit}>
-                <h2>Settings for control {controlId}</h2>
+                <h2>Settings for control</h2>
                 <div className="form-group">
                     <label>Effects block</label>
-                    <select defaultValue={blockId && blockId.toString()} onChange={event => this.setValue('blockId', event.target.value)}>
-                        <option disabled value="-1">Select effects block</option>
+                    <select value={blockId && blockId.toString() || ''} onChange={event => this.setValue('blockId', event.target.value)}>
+                        <option disabled value="">Select effects block</option>
                         {blocks.map((block, i) => (
                             <option key={`block-${i}`} value={block.id}>{block.label}</option>
                         ))}
@@ -84,8 +86,8 @@ export default class ControlEditorComponent extends React.Component<Props, State
                 </div>
                 <div className="form-group">
                     <label>Parameter to control</label>
-                    <select defaultValue={paramId && paramId.toString()} disabled={!selectedBlock} onChange={event => this.setValue('paramId', event.target.value)}>
-                        <option disabled value="-1">Select effects param</option>
+                    <select value={paramId && paramId.toString() || ''} disabled={!selectedBlock} onChange={event => this.setValue('paramId', event.target.value)}>
+                        <option disabled value="">Select effects param</option>
                         {selectedBlock && selectedBlock.parameters.map((param, i) => (
                             <option key={`param-${i}`} value={param.id}>{param.label}</option>
                         ))}
@@ -111,12 +113,17 @@ export default class ControlEditorComponent extends React.Component<Props, State
                 </div>
                 <div className="form-group">
                     <label>CC that controls parameter</label>
-                    <input type="number" name="controlChange" defaultValue={cc !== null && cc.toString()} onChange={event => this.setValue('cc', event.target.value)} />
+                    <input type="number" 
+                        name="controlChange"
+                        min="0"
+                        max="127"
+                        defaultValue={cc !== null && cc.toString()}
+                        onChange={event => this.setValue('cc', event.target.value)} />
                 </div>
                 
                 <div className="control-editor__actions">
                     <button className="btn" onClick={() => this.onCancel()}>Cancel</button>
-                    <input type="submit" value="Done" className="btn btn--primary" />
+                    <input type="submit" value="Done" disabled={!hasChanges} className="btn btn--primary" />
                 </div>
             </form>
         );
