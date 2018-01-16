ff3dd3b3aa40bec9dbe1184719763c4ffb7bd502
Mikko Lakomaa <hopealusikka@gmail.com>
2018-01-16 22:09:47 +0200

    Various changes to actions and reducers

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -3,27 +3,33 @@ import { AxeFx } from "../api/axefx";
 import { MIDIController, MIDIDeviceData } from "../api/midi";
 import { PanelObject } from "../api/panel-object";
 import { GenericMIDIController } from "../api/generic-midi-controller";
+import { FxBlock } from "../api/fx-block";
 
 export enum TypeKeys {
-    getPanel = 'Get single panel',
-    setPanel = 'Set panel',
+    loading = 'Loading data',
+    getCurrentPanel = 'Get current panel',
+    setPanel = 'Set panel data',
     setMIDIDeviceData = 'Set MIDI device data',
+    setBlocks = 'Set effects blocks',
+    updateControlValue = 'Update control value',
     updateAxeFx = 'Update Axe-Fx',
-    resetAxeFx = 'Reset Axe-Fx'
+    resetAxeFx = 'Reset Axe-Fx',
+    updateController = 'Update MIDI controller'
 };
 
-export interface SetMIDIDeviceData extends Action {
-    payload: MIDIDeviceData
-}
-export const setMIDIDeviceData = (payload): SetMIDIDeviceData => ({
+export interface ActionWithPayload extends Action { payload: any }
+
+export const loadingAction = (payload: boolean) : ActionWithPayload => ({
+    type: TypeKeys.loading,
+    payload
+});
+
+export const setMIDIDeviceDataAction = (payload) => ({
     type: TypeKeys.setMIDIDeviceData,
     payload
 });
 
-export interface AxeFxUpdateAction extends Action {
-    payload: [string, any]
-}
-export const axeFxUpdateAction = (payload): AxeFxUpdateAction => ({
+export const axeFxUpdateAction = (payload): ActionWithPayload => ({
     type: TypeKeys.updateAxeFx,
     payload: payload
 });
@@ -32,24 +38,28 @@ export const axeFxResetAction = (): Action => ({
     type: TypeKeys.resetAxeFx,
 });
 
-export interface GetPanelAction extends Action {
-    payload: number;
-}
-export const getPanelAction = (payload): GetPanelAction => ({
-    type: TypeKeys.getPanel,
+export const updateControlValueAction = (payload:  {
+    blockId: number,
+    paramId: number,
+    paramValue: number
+}): ActionWithPayload => ({
+    type: TypeKeys.updateControlValue,
+    payload
+});
+
+export const getCurrentPanelAction = (payload: number): ActionWithPayload => ({
+    type: TypeKeys.getCurrentPanel,
     payload
 })
 
-export interface SetPanelAction extends Action {
-    payload: PanelObject
-}
-export const setPanelAction = (payload): SetPanelAction => ({
+export const setPanelAction = (payload: PanelObject): ActionWithPayload => ({
     type: TypeKeys.setPanel,
     payload
 });
 
-export type ActionTypes = 
-    | AxeFxUpdateAction
-    | SetMIDIDeviceData
-    | SetPanelAction
-    | GetPanelAction;
+export const updateControllerAction = (payload): ActionWithPayload => ({
+    type: TypeKeys.updateController,
+    payload
+});
+
+export type ActionTypes = Action | ActionWithPayload;
--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -3,9 +3,11 @@ import { ActionTypes, TypeKeys } from './actions';
 import { PanelObject } from '../api/panel-object';
 import { AxeFx, AxeFxState } from '../api/axefx';
 import { MIDIController, MIDIDeviceData, MIDIControllerType } from '../api/midi';
+import { ControllerState } from '../api/generic-midi-controller';
 
 interface State {
     axeFx: AxeFxState,
+    controller: ControllerState,
     devices: MIDIDeviceData[];
     currentPanel: PanelObject;
     panels: PanelObject[];
@@ -20,6 +22,9 @@ const initialState: State = {
         currentPresetName: null,
         currentPresetNumber: null
     },
+    controller: {
+        name: null
+    },
     devices: [],
     currentPanel: null,
     panels: []
@@ -27,34 +32,45 @@ const initialState: State = {
 
 export default function reducers(state = initialState, action: Action & { payload: any }) {
     switch(action.type) {
+        case TypeKeys.loading:
+            return {...state, loading: action.payload};
         case TypeKeys.updateAxeFx:
-            return {...state, axeFx: {...state.axeFx, ...action.payload}}
+            return {...state, axeFx: {...state.axeFx, ...action.payload}};
         case TypeKeys.resetAxeFx:
-            return {...state, axeFx: initialState.axeFx}
+            return {...state, axeFx: initialState.axeFx};
+        case TypeKeys.updateControlValue:
+            const { blockId, paramId, paramValue } = action.payload;
+            const { currentPanel } = state;
+            const controlIdx = currentPanel.controls.findIndex(ctrl => ctrl.blockId === blockId && ctrl.paramId === paramId);
+            const updatedPanel = {
+                ...currentPanel,
+                controls: currentPanel.controls.map((ctrl, i) => (i === controlIdx) ? {...ctrl, paramValue} : ctrl)
+            };
+            return {...state, currentPanel: updatedPanel };
         case TypeKeys.setMIDIDeviceData:
             const existingDeviceIdx = state.devices.findIndex(device => device.id === action.payload.id);
             if (existingDeviceIdx !== -1) {
                 // Update existing device
-                state.devices[existingDeviceIdx] = action.payload;
-                return {...state, devices: [...state.devices]}
+                return {...state, devices: state.devices.map((device, i) => i === existingDeviceIdx ? action.payload : device)};
             } else {
                 // Add new device
-                return {...state, devices: [...state.devices, action.payload]}
+                return {...state, devices: [...state.devices, action.payload]};
             }
-        case TypeKeys.getPanel:
+        case TypeKeys.getCurrentPanel:
             const panel = state.panels.find(panel => panel.id === action.payload);
             if (!panel) return {...state, currentPanel: null};
-            return {...state, currentPanel: Object.assign({}, panel)}
+            return {...state, currentPanel: {...panel}};
         case TypeKeys.setPanel:
             const existingPanelIdx = state.panels.findIndex(panel => panel.id === action.payload.id);
             if (existingPanelIdx !== -1) {
                 // Update existing panel
-                state.panels[existingPanelIdx] = action.payload;
-                return {...state, panels: [...state.panels]}
+                return {...state, panels: state.panels.map((panel, i) => i === existingPanelIdx ? action.payload : panel)};
             } else {
                 // Add new panel
-                return {...state, panels: [...state.panels, action.payload]}
+                return {...state, panels: [...state.panels, action.payload]};
             }
+        case TypeKeys.updateController:
+            return {...state, controller: {...state.controller, ...action.payload}};
         default:
             return state;
     }
