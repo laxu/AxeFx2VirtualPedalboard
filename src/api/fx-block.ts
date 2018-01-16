ff4cdcb924bc23d245bb4edd64476426a0978021
Mikko Lakomaa <hopealusikka@gmail.com>
2018-01-16 22:06:45 +0200

    Change fxparam to have a formatValue function instead of setting a value for it

--- src/api/fx-block.ts
+++ src/api/fx-block.ts
@@ -13,10 +13,10 @@ export interface IFxParam {
     blockGroup: string;
     id: number;
     type: PARAM_TYPE;
-    value: number;
     step?: number;
     range?: [number, number];
     label?: string;
+    formatValue: (value: number) => number;
 }
 
 export class FxBlock implements IFxBlock {
@@ -37,18 +37,6 @@ export class FxBlock implements IFxBlock {
     getParam(paramId: number): FxParam {
        return this.parameters.find(param => param.id === paramId) || null;
     }
-
-    getParamValue(paramId: number): number {
-        const param = this.getParam(paramId);
-        if (!param) return null;
-        return param.value;
-    }
-
-    setParamValue(paramId: number, paramValue: number): void {
-        const param = this.parameters.find(param => param.id === paramId);
-        if (!param) throw new Error('Cannot set value of non-existent param!');
-        param.value = paramValue;
-    }
 }
 
 export class FxParam implements IFxParam {
@@ -68,9 +56,9 @@ export class FxParam implements IFxParam {
         this.label = paramSettings.label;
     }
 
-    set value(val: number) {
+    formatValue(val: number): number {
         if (!isFinite(val)) throw new Error('Trying to set non-numeric param value!');
-        this.value = clampValue(val, this.range, this.step);
+        return clampValue(val, this.range, this.step);
     }
 }
 
