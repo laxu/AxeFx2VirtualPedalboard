45120a0c3a94c64eb94115598283435b6f50b76b
Mikko Lakomaa <hopealusikka@gmail.com>
2018-01-18 20:40:06 +0200

    Add debounce function

--- src/util/util.ts
+++ src/util/util.ts
@@ -111,3 +111,19 @@ export function handleSubmit(callback) {
         return false;
     }
 }
+
+export function debounce(func, wait: number, immediate: boolean = false) {
+	let timeout;
+	return function() {
+        const context = this;
+        const args = arguments;
+		const later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		const callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
