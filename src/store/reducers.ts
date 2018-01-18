4e2559f3a193d4aa9465f30d8f9c17211f2aede5
Mikko Lakomaa <hopealusikka@gmail.com>
2018-01-18 22:28:24 +0200

    Various tweaks to reducers

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -10,6 +10,7 @@ interface State {
     controller: ControllerState,
     devices: MIDIDeviceData[];
     currentPanel: PanelObject;
+    shouldRefreshCurrentPanel: boolean;
     panels: PanelObject[];
 }
 
@@ -23,23 +24,29 @@ const initialState: State = {
         currentPresetNumber: null
     },
     controller: {
-        name: null
+        name: null,
+        connected: false
     },
     devices: [],
     currentPanel: null,
+    shouldRefreshCurrentPanel: false,
     panels: []
 };
 
 export default function reducers(state = initialState, action: Action & { payload: any }) {
+    const payload = action.payload;
     switch(action.type) {
         case TypeKeys.loading:
-            return {...state, loading: action.payload};
+            return {...state, loading: payload};
+
         case TypeKeys.updateAxeFx:
-            return {...state, axeFx: {...state.axeFx, ...action.payload}};
+            return {...state, axeFx: {...state.axeFx, ...payload}};
+
         case TypeKeys.resetAxeFx:
             return {...state, axeFx: initialState.axeFx};
+
         case TypeKeys.updateControlValue:
-            const { blockId, paramId, paramValue } = action.payload;
+            const { blockId, paramId, paramValue } = payload;
             const { currentPanel } = state;
             const controlIdx = currentPanel.controls.findIndex(ctrl => ctrl.blockId === blockId && ctrl.paramId === paramId);
             const updatedPanel = {
@@ -47,30 +54,48 @@ export default function reducers(state = initialState, action: Action & { payloa
                 controls: currentPanel.controls.map((ctrl, i) => (i === controlIdx) ? {...ctrl, paramValue} : ctrl)
             };
             return {...state, currentPanel: updatedPanel };
+
+        case TypeKeys.resetControlValues:
+            return {
+                ...state,
+                panels: state.panels.map(panel => ({
+                    ...panel,
+                    controls: panel.controls.map(ctrl => ({...ctrl, paramValue: null}))
+                }))
+            };
+
         case TypeKeys.setMIDIDeviceData:
-            const existingDeviceIdx = state.devices.findIndex(device => device.id === action.payload.id);
+            const existingDeviceIdx = state.devices.findIndex(device => device.id === payload.id);
             if (existingDeviceIdx !== -1) {
                 // Update existing device
-                return {...state, devices: state.devices.map((device, i) => i === existingDeviceIdx ? action.payload : device)};
+                return {...state, devices: state.devices.map((device, i) => i === existingDeviceIdx ? {...device, ...payload} : device)};
             } else {
                 // Add new device
-                return {...state, devices: [...state.devices, action.payload]};
+                return {...state, devices: [...state.devices, payload]};
             }
+
         case TypeKeys.getCurrentPanel:
-            const panel = state.panels.find(panel => panel.id === action.payload);
+            const panel = state.panels.find(panel => panel.id === payload);
             if (!panel) return {...state, currentPanel: null};
             return {...state, currentPanel: {...panel}};
+
         case TypeKeys.setPanel:
-            const existingPanelIdx = state.panels.findIndex(panel => panel.id === action.payload.id);
+            const existingPanelIdx = state.panels.findIndex(panel => panel.id === payload.id);
             if (existingPanelIdx !== -1) {
                 // Update existing panel
-                return {...state, panels: state.panels.map((panel, i) => i === existingPanelIdx ? action.payload : panel)};
+                console.log('updating panel', state.panels.find(panel => panel.id === payload.id), payload);
+                return {...state, panels: state.panels.map((panel, i) => i === existingPanelIdx ? {...panel, ...payload} : panel)};
             } else {
                 // Add new panel
-                return {...state, panels: [...state.panels, action.payload]};
+                return {...state, panels: [...state.panels, payload]};
             }
+
+        case TypeKeys.refreshCurrentPanel:
+            return {...state, shouldRefreshCurrentPanel: payload};
+
         case TypeKeys.updateController:
-            return {...state, controller: {...state.controller, ...action.payload}};
+            return {...state, controller: {...state.controller, ...payload}};
+
         default:
             return state;
     }
