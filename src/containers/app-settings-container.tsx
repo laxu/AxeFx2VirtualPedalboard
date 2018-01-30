b4d06a64d103ef5c809490a82e497e77c093a4fd
Mikko Lakomaa <hopealusikka@gmail.com>
2018-01-30 21:26:23 +0200

    Tweaks to app settings saving

--- src/containers/app-settings-container.tsx
+++ src/containers/app-settings-container.tsx
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import { setMIDIDeviceDataAction } from '../store/actions';
-import { WebMidiWrapper, MIDIInput, MIDIOutput, MIDIControllerType, MIDIDeviceData, updateDevices } from '../api/midi';
+import { WebMidiWrapper, MIDIInput, MIDIOutput, MIDIControllerType, MIDIDeviceData, updateDevices, getAxeFxInstance, getControllerInstance } from '../api/midi';
 import { AxeFx } from '../api/axefx';
 import { GenericMIDIController } from '../api/generic-midi-controller';
 import AppSettingsComponent from '../components/app-settings/app-settings';
@@ -17,25 +17,27 @@ const mergeProps = (stateProps, dispatchProps, ownProps) => ({
     ...ownProps,
     saveChanges(formData) {
         const { dispatch } = dispatchProps;
+        const axeFx = getAxeFxInstance();
+        const controller = getControllerInstance();
         const devices: MIDIDeviceData[] = [
             {
-                id: 'axeFx',
+                id: axeFx.id,
                 type: MIDIControllerType.AxeFx,
                 inputName: formData.axeFxInput,
                 outputName: formData.axeFxOutput,
                 channel: formData.axeFxChannel === 'all' ? 'all': Number(formData.axeFxChannel)
             },
             {
-                id: generateId(),
+                id: controller.id,
                 type: MIDIControllerType.Controller,
                 inputName: formData.controllerInput,
                 outputName: formData.controllerOutput,
-                channel: formData.axeFxChannel === 'all' ? 'all': Number(formData.axeFxChannel)
+                channel: formData.controllerChannel === 'all' ? 'all': Number(formData.axeFxChannel)
             },
         ]
 
         devices.map(device => dispatch(setMIDIDeviceDataAction(device)));
-        updateDevices(devices, dispatch);
+        updateDevices(devices);
     }
 });
 
