95ba29ed2a5661fc426e4395d1f7763cd26f367e
Mikko Lakomaa <hopealusikka@gmail.com>
2018-02-27 00:22:50 +0200

    Add getBypassState value function

--- src/api/fx-block-data/fx-param-common.ts
+++ src/api/fx-block-data/fx-param-common.ts
@@ -4,6 +4,10 @@ export enum PARAM_TYPE {
     Knob = 'knob'
 };
 
+export function getBypassState(value: number): boolean {
+    return !value;
+}
+
 export const FX_PARAMS_COMMON = [
-    { id: 255, label: 'Engage/bypass block', type: PARAM_TYPE.Switch }
+    { id: 255, label: 'Engage / bypass block', type: PARAM_TYPE.Switch, valueFunc: getBypassState }
 ];
