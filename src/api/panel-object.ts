2cd1f52ab2661061aca34e9d4e90db0e7ec2a2e6
Mikko Lakomaa <hopealusikka@gmail.com>
2018-04-15 15:38:46 +0300

    Add groups to PanelObject

--- src/api/panel-object.ts
+++ src/api/panel-object.ts
@@ -1,10 +1,12 @@
 import { ControlObject } from "./control-object";
 import { MIDIController } from "./midi";
+import { GroupObject } from "./group-object";
 
 export interface PanelObject {
     id: string;
     label: string;
     controllerId: string;
     controls: ControlObject[];
+    groups: GroupObject[];
     cc?: number;
 }
