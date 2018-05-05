8b7b027a17ebb0fe25b80de9173d0f8cc3b2a51f
Mikko Lakomaa <hopealusikka@gmail.com>
2018-05-05 15:05:36 +0300

    Don't re-select same board if clicking on its name

--- src/containers/app-container.tsx
+++ src/containers/app-container.tsx
@@ -68,6 +68,7 @@ const mergeProps = (stateProps, dispatchProps, ownProps) => {
             dispatch(editBoardAction(board));
         },
         selectBoard(board) {
+            if (board.id === currentBoard.id) return;
             dispatch(setCurrentBoardAction(board.id));
         }
     }
